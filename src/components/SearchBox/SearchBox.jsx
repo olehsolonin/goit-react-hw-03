a6ebcc3bb89e9b47672632714a3dd52fb5fffca7
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-07-06 22:59:21 +0300

    hw3-new-fix

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,14 @@
+import css from './SearchBox.module.css';
+
 export default function SearchBox({ filter, onFilter }) {
   return (
-    <div>
+    <div className={css.filter}>
       <input
         type="text"
         value={filter}
         onChange={e => onFilter(e.target.value)}
+        className={css.filterInput}
+        placeholder="Search user"
       />
     </div>
   );
